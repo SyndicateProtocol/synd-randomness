3d51d455ce06be35afd14b82eb2a09eca8ee0380
caleb <caleb@syndicate.io>
2025-10-17 18:14:24 -0500

    Random.sol tests

---
+++ contracts/test/Random.t.sol
@@ -0,0 +1,120 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {Test} from "forge-std/Test.sol";
+import {Random} from "../src/Random.sol";
+import {IAccessControl} from "@openzeppelin/contracts/access/IAccessControl.sol";
+
+contract RandomTest is Test {
+    Random public randomContract;
+
+    address public randomnessAdmin = address(1);
+    address public unauthorized = address(2);
+
+    function setUp() public {
+        randomContract = new Random(randomnessAdmin);
+    }
+
+    function testConstructor() public view {
+        assertTrue(randomContract.hasRole(randomContract.DEFAULT_ADMIN_ROLE(), randomnessAdmin));
+        assertTrue(randomContract.hasRole(randomContract.RANDOMNESS_ADMIN_ROLE(), randomnessAdmin));
+    }
+
+    function testSetRandom() public {
+        uint256 newRandom = 123456789;
+
+        vm.prank(randomnessAdmin);
+        randomContract.setRandom(newRandom);
+
+        assertEq(randomContract.random(), newRandom);
+    }
+
+    function testSetRandomUnauthorized() public {
+        uint256 newRandom = 123456789;
+
+        vm.prank(unauthorized);
+        vm.expectRevert("Caller is not the randomness admin");
+        randomContract.setRandom(newRandom);
+    }
+
+    function testSetRandomMultipleTimes() public {
+        vm.startPrank(randomnessAdmin);
+
+        randomContract.setRandom(111);
+        assertEq(randomContract.random(), 111);
+
+        randomContract.setRandom(222);
+        assertEq(randomContract.random(), 222);
+
+        randomContract.setRandom(333);
+        assertEq(randomContract.random(), 333);
+
+        vm.stopPrank();
+    }
+
+    function testFuzzSetRandom(uint256 randomValue) public {
+        vm.prank(randomnessAdmin);
+        randomContract.setRandom(randomValue);
+
+        assertEq(randomContract.random(), randomValue);
+    }
+
+    function testRandomDefaultValue() public view {
+        assertEq(randomContract.random(), 0);
+    }
+
+    function testGrantRandomnessAdminRole() public {
+        address newAdmin = address(3);
+
+        vm.startPrank(randomnessAdmin);
+        randomContract.grantRole(randomContract.RANDOMNESS_ADMIN_ROLE(), newAdmin);
+        vm.stopPrank();
+
+        assertTrue(randomContract.hasRole(randomContract.RANDOMNESS_ADMIN_ROLE(), newAdmin));
+
+        // New admin should be able to set random
+        vm.prank(newAdmin);
+        randomContract.setRandom(999);
+        assertEq(randomContract.random(), 999);
+    }
+
+    function testRevokeRandomnessAdminRole() public {
+        vm.startPrank(randomnessAdmin);
+        randomContract.revokeRole(randomContract.RANDOMNESS_ADMIN_ROLE(), randomnessAdmin);
+        vm.stopPrank();
+
+        assertFalse(randomContract.hasRole(randomContract.RANDOMNESS_ADMIN_ROLE(), randomnessAdmin));
+
+        // Revoked admin should not be able to set random
+        vm.prank(randomnessAdmin);
+        vm.expectRevert("Caller is not the randomness admin");
+        randomContract.setRandom(123);
+    }
+
+    function testMultipleAdmins() public {
+        address admin2 = address(3);
+        address admin3 = address(4);
+
+        vm.startPrank(randomnessAdmin);
+        randomContract.grantRole(randomContract.RANDOMNESS_ADMIN_ROLE(), admin2);
+        randomContract.grantRole(randomContract.RANDOMNESS_ADMIN_ROLE(), admin3);
+        vm.stopPrank();
+
+        // All admins can set random
+        vm.prank(randomnessAdmin);
+        randomContract.setRandom(100);
+        assertEq(randomContract.random(), 100);
+
+        vm.prank(admin2);
+        randomContract.setRandom(200);
+        assertEq(randomContract.random(), 200);
+
+        vm.prank(admin3);
+        randomContract.setRandom(300);
+        assertEq(randomContract.random(), 300);
+    }
+
+    function testRandomnessAdminRoleValue() public view {
+        assertEq(randomContract.RANDOMNESS_ADMIN_ROLE(), keccak256("RANDOMNESS_ADMIN_ROLE"));
+    }
+}
